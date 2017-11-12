bcd3ec9a2c3b6f92411b301ecd6daa3a34add8b7
Amecom <32417424+Amecom@users.noreply.github.com>
2017-11-12 03:33:41 +0100

    rinominato

--- README.md
+++ README.md
@@ -1,1 +1,164 @@
 # S32Server
+
+S32Server (S3 To Server) è uno script per sincronizzare il contenuto di
+uno o più percorsì direcory di un computer su 
+un altro computer utilizzando come middleware il servizio AWS S3.
+
+Il programma può essere utilizzato in modalità 'master' o 'slave'.
+Il computer 'master' contiene i file originali che vengono copiati su AWS S3
+mentre il computer 'slave' copia localmente i file memorizzati su AWS S3.
+
+Percorso files:
+	MASTER >> S3 >> SLAVE
+
+Dato che i computer master e slave non cominucano direttamente tra loro
+non è necessario che esista uno script master su di un computer affichè
+un slave possa funzioanare. Ad esempio io utilizzo questo script solo
+im modalità salve sui server che voglio aggiornare mentre utilizzo
+un altro applicativo che mi mantiene sincronizzato in tempo reale
+i dati tra il mio pc e il percorso su AWS S3.
+
+
+
+# Requisiti
+
+Lo script è stato testato con Python3.6
+
+È necessario disporre di credenziali di di accesso allo storage AWS S3
+e configurare il computer per l'utilizzo della libreria BOTO3
+tramite la creazione del file:
+
+~/.aws/credentials
+
+Il cui contenuto è simile questo:
+
+[default]
+aws_access_key_id = MY_ACCESS_KEY
+aws_secret_access_key = MY_SECRET_KEY
+
+Per maggiori informazioni a riguardo consultare la guida a BOTO3 offerta da AWS
+
+
+
+
+# Creazione del file di mappatura
+
+Perchè lo script funzioni è necessario creare uno, o più file,
+contenenti le informazioni di sincronizzazione. 
+Le informazioni mappano i percorsi tra master > s3 > slave
+che possono esseri diversi.
+
+Esempio file 'syncro.json':
+
+[
+    {
+      "name": "PROJECT 1",
+      "description": "OPTIONAL PROJECT DESCRIPTION",
+      "master": "c:/path/dir/master/1",
+      "s3": "bucketname/backup/dir1",
+      "slave": "/path/slave/dir1"
+    },
+    {
+      "name": "PROJECT 2",
+      "description": "OPTIONAL PROJECT DESCRIPTION",
+      "master": "c:/path/dir/master/2",
+      "s3": "bucketname/dir/dir/dir",
+      "slave": "/path/slave/dir2",
+	  "files": ["filename_1", "filename_2"]
+    }
+]
+
+
+In questo esempio ho creato una mappa chiamta "syncro.json" che contiene due percorsi chiamati 
+'PROJECT 1' e 'PROJECT 2'.
+
+Il computer master è OS Windows, mentre il computer slave è Linux.
+
+La proprietà 'master' è necessaria solo se lo script viene eseguito in modalità master
+e contiene il percorso della directory di origine dei file della macchina master.
+Se non viene utilizzato lo script in modalità master è possibile omettere questa proprietà.
+
+La proprietà 's3' contiene il percorso su AWS S3 e deve essere sempre presente. 
+In questo percorso vengono caricati i file dal computer master e prelevati dal computer slave. 
+
+La proprietà 'slave' è necessaria solo se lo script viene eseguito in modalità slave
+e contiene il percorso locale dove verranno copiati i file.
+Se non viene utilizzato lo script in modalità slave è possibile omettere questa proprietà.
+
+In 'PROJECT 2' tramite la proprietà 'files'
+sono specificati i file che si vuole copiare, mentre in 'PROJECT 1'
+verranno sincronizzati tutti i file presenti nel percorso master su S3 e tutti i file presenti nel percorso S3 su slave.
+
+ATTENZIONE: se la directory specificata in 'slave' non esiste verrà creata, mentre se esiste cancellata
+e ricreata quindi i vecchi file verranno cancellati.
+In 'PROJECT 2' ad esempio dopo la sinctronizzazione la directory "/path/server/dir2" conterrà solo
+i file "filename_1", "filename_2" come specificato in 'files';
+eventuali file presenti in precedenza nella directory verranno cancellati.
+
+
+# Salvataggio dei file mappatura
+
+Il file creato nel paragrafo precedente dovrà essere salvato in una directory su AWS S3
+per essere accessibile sia dal computer master che slave.
+Creo bucket e una cartella nella quale inserisco il file.
+
+Quindi nel percorso:
+
+bucketname/S32SMAP/foo/data
+
+carico il file 'syncro.json'.
+
+È possibile inserire più mappe all'interno della directory.
+
+
+# Installazione dello script u.py
+
+Per installare sul server lo script 
+è sufficiente scaricare il file tramite un comando wget:
+
+$ wget https://raw.githubusercontent.com/Amecom/S32Server/master/u.py
+
+(Nota: il fatto che il percorso del file contenga la parola master è una coincidenza che nulla
+ha a che vedere con il concetto di 'master' e 'slave' discusso fino ad ora)
+
+
+# Esecuzione dello script
+
+La prima volta che lo script viene eseguito verrà chiesto:
+
+1) Come configurare il computer: master o slave.
+
+2) Di inserire il percorso S3 dove sono
+state salvate le definizioni di sincronizzazione per seguire l'esempio
+come riportato fino ad ora dovrò inserire: 
+
+bucketname/S32SMAP/nomeserver/data
+
+
+Se tutti i passaggi sono stati eseguiti correttamente (credenziali BOTO3 corrette, presenza delle definizioni, etc)
+lo script mi fornisce l'interfaccia pr eseguire la sincronizzazione dei dati.
+
+
+# Aggiornare i file di mappatura
+
+È possibile che nel tempo si effettuino delle modifiche nei percorsi di mappatura 
+tuttavia le mappe vengono copiate localmente e nel caso si modifichi il file 'syncro.json' su AWS S3
+è necessario aggiornare esplicitamente la copia locale.
+Questa operazione è fornita dall'interfaccia dello script.
+
+
+# File 'config.json'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ s32s.py
@@ -0,0 +1,473 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import boto3
+import shutil
+import json
+import subprocess
+from time import sleep
+
+MASTER = 0
+SLAVE = 1
+
+# CONFIGURAZIONE
+# ############################################
+# NON MODIFICARE IL FILE DI CONFIGURAZIONE QUI.
+# Creare un file config.json con la propria configurazione
+# I valori che seguono sono le proprietà di default
+# ############################################
+START_CONFIG = {
+    # Url di aggiornamento script
+    "S3PScript" : "https://raw.githubusercontent.com/Amecom/S32Server/master/s32s.py",
+
+    # Percorsi delle directory S3 nelle quali sono memorizzate le
+    # definizioni di sincronizzazione e che è possibile caricare 
+    "S3PDef" : None,
+
+    # Specifica se riordinare alfabeticamente i percorsi
+    "ReorderPath": True,
+
+    # Includere il comando service HTTPD restart
+    "AddRH" : False,
+
+    # Se True non viene mostarto l'avviso di cancellazione dei file locali
+    "SkipDeleteAlert": False
+}
+
+# creazione del client S3, utilizzando le credenziali
+# salvate in ~/.aws/credentials
+S3CLIENT = boto3.client('s3', config = boto3.session.Config(signature_version = 's3v4'))
+S3RESOURCE =  boto3.resource('s3', config=boto3.session.Config(signature_version='s3v4'))
+
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join( CURRENT_PATH, "DATA")
+STR_CONTINUE = "\nDone. Press ENTER to continue... "
+CONFIG = None
+
+def clear_screen():
+    """Pulisce lo schermo"""
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def filter_files(data:list):
+    """Regole di esclusione
+    dalla sincronizzazione file.
+    In questo esempio escludo sempre i file della cache di Python
+    """
+    return [ d for d in data if d.find("__pycache__") == -1 ]
+
+def build_dir(path, chiedi=False):
+    # Non deve eliminare niente, ma creare solo le directory 
+    # che eventualmente mancano per 'path'
+    tree = path.split("/")
+    p = []
+    for a in tree:
+        p.append(a)
+        # controllo che il primo percorso non sia vuoto (in linux succede)
+        if len(a) > 0:
+            dir = "/".join(p)
+            if not os.path.isdir( dir ):
+                if chiedi:
+                    z = input("Create '{}' y/n? ".format(dir) ).lower()
+                else:
+                    z = "y"
+
+                if z != "y":
+                    raise "impossibile continuare senza creare la directory"
+                print("MKDIR: {}".format( dir ) )
+                os.mkdir( dir )
+
+def split_bucket_prefix(path):
+    s3_part = path.split("/")
+    bucket = s3_part[0]
+    prefix = "/".join( s3_part[1:] )
+    return bucket, prefix
+
+
+def s3_ls(path):
+    """
+    Restituisce la lista dei percorsi relativi
+    dei file presenti nel path S3 specificato.
+    """
+    bucket, prefix = split_bucket_prefix(path)
+    objects = S3CLIENT.list_objects_v2( Bucket=bucket, Prefix=prefix)
+    files = []
+    for object in objects["Contents"]:
+        k = object["Key"]
+        # escludo le directory
+        if k[-1] != "/":
+            # rendo il percorso relativo al prefix
+            #  (+1 per rimuovere la '/' )
+            files.append( object["Key"][len(prefix)+1:] )
+    return files
+
+def s3_del(path):
+    print("S3 DEL: {} ... ".format(path), end="")
+
+    bucket, prefix = split_bucket_prefix(path)
+    b = S3RESOURCE.Bucket(bucket)
+    try:
+        request = b.objects.filter(Prefix=prefix).delete()
+    except Exception as e:
+        print( "ERROR" )
+        print(e)
+        return False
+    else:
+        print( "OK" )
+        return True
+
+def s3_create(path, data=None):
+    bucket, prefix = split_bucket_prefix(path)
+
+    print("S3 CREATE: {} ... ".format( prefix ) , end="")
+    object = S3RESOURCE.Object( bucket, prefix )
+
+
+    if data is None:
+        # crea directory
+        assert path[-1] == "/", "If data is None, path must be a directory. Directory end with /"
+        request = object.put()
+    else:
+        request = object.put(Body=data)
+    
+    result = request['ResponseMetadata']['HTTPStatusCode'] == 200
+    print( result )
+    return result
+
+
+def s3_to_local(s3path, local_path, listobjects):
+
+    bucket, prefix = split_bucket_prefix(s3path)
+
+    print("\nEXECUTE COPY...\n * From S3\t\t: {}\n * To Local Path\t: {}\n".format(
+                s3path,
+                local_path
+            )
+          )
+
+    confirm = "y" if CONFIG["SkipDeleteAlert"] else input("*** ATTENTION ***\nALL FILES in '{}' WILL BE PERMANENTLY DELETE.\n\nType 'y' end press ENTER to confirm: ".format(local_path)).lower()
+
+    if confirm == "y":
+        # elimino il vecchio contenuto
+        if os.path.isdir(local_path):
+            print("DEL: {}".format( local_path ))
+            shutil.rmtree(local_path)
+            # e' utile fare una pausa prima di ricreare una directory appena eliminata
+            # per permettere al sistema di rilasciare i files
+            sleep(3)
+
+        build_dir(local_path)
+
+        for file in listobjects:
+            file_local_path = os.path.join( local_path, file )
+            # COSTRUISCO DIRECTORY LOCALE
+            file_tree = file_local_path.split("/")
+            # in linux il primo percosro è lungezza 0
+            # quindi evito con un controllo sulla lunghezza
+            if len(file_tree) > 1:
+                p = "/".join( file_tree[0:-1] )
+                build_dir(p)
+            # DOWNLOAD
+            try:
+                print("COPY: {}".format( file_local_path ) )
+                S3CLIENT.download_file( bucket, prefix + "/" + file, file_local_path )
+            except Exception as e:
+                print(e)
+                break
+    else:
+        print("NOTHING DO")
+
+def master_to_s3(s3path, local_path, listobject):
+    bucket, prefix = split_bucket_prefix(s3path)
+
+    print("\nEXECUTE COPY...\n * From Local Path\t: {}\n * To S3\t\t: {}\n".format(
+                local_path,
+                s3path
+            )
+          )
+
+    confirm = "y" if CONFIG["SkipDeleteAlert"] else input("*** ATTENTION ***\nALL FILES in AWS S3 '{}' WILL BE PERMANENTLY DELETE.\n\nType 'y' end press ENTER to confirm: ".format(s3path)).lower()
+    if confirm == "y":
+
+        if s3_del(s3path):
+            if s3_create(s3path + "/"):
+                for f in listobject:
+                    data = None
+                    print(f , "is", end= " ")
+                    if f[-1] != "/":
+                        print( "FILE")
+                        with open( os.path.join( local_path, f), "rb" ) as bin:
+                            data = bin.read()
+                    else:
+                        print( "DIR")
+                    s3_create("/".join( [bucket, prefix, f]), data )
+
+    else:
+        print("NOTHING DO")
+
+    pass
+
+
+def update_script():
+    bucket, prefix = split_bucket_prefix( CONFIG['S3PScript'] )
+    S3CLIENT.download_file( bucket, prefix, CURRENT_PATH + "/u.py" )
+
+
+def update_maps():
+
+    while True:
+
+        print( "\nUPDATE MAPS\n")
+
+        if CONFIG['S3PDef']:
+            for n, percorso in enumerate( CONFIG['S3PDef']):
+                print( " {} = {}".format(n, percorso['name']) )
+            print("")
+            print("."*50)
+            print("")
+
+        print( " m = S3 Path")
+        print( " x = Exit")
+
+        s = input( "\nSelect: ").lower()
+
+        if s == "x":
+            return
+
+        if s == "m":
+            s3input = input("Insert S3 Path with map json files (x to Exit): ")
+            if s3input.lower() == "x":
+                return
+            print("............")
+        else:
+            try:
+                s = int(s)
+            except:
+                continue
+            else:
+
+                if s >= 0 and s < len( CONFIG['S3PDef'] ):
+                    s3input = CONFIG['S3PDef'][s]['s3']
+                else:
+                    continue
+
+        files = filter_files( s3_ls(s3input) )
+        s3_to_local(s3input, DATA_PATH, files)
+        return
+
+def load_configuration():
+    p = os.path.join( CURRENT_PATH, "config.json")
+    if os.path.exists(p):
+        with open( p, "r") as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    for k , v in START_CONFIG.items():
+        if not data.get(k):
+            data[k] = v
+    return data
+
+
+def load_maps():
+    json_data = []
+    for file in os.listdir(DATA_PATH):
+        if file.endswith(".json"):
+            with open( os.path.join(DATA_PATH, file), "r") as f:
+                jsd = json.load(f)
+                if CONFIG['ReorderPath']:
+                    jsd = sorted(jsd, key=lambda k:k['name'])
+
+                for data in jsd:
+                    data['filename'] = file[:-5]
+                    json_data.append(data)
+    return json_data
+
+
+def syncronize(data, mode):
+
+    files = data.get('files')
+
+    if mode == SLAVE:
+        # se nella definizione è presente un lista di files
+        # sincronizzo solo quelli
+        # ATTENZIONE eventuali altri file presenti localmenti nella directory verranno comunque rimossi!
+        # Altrimenti sincronizzo tutti i file presenti nel percorso S3 specificato
+        if not files:
+            files = filter_files( s3_ls(data['s3']) )
+        s3_to_local(data['s3'], data['slave'], files)
+
+    elif mode == MASTER:
+        local_path = data['master'].replace("\\","/")
+        if not files:
+            print("WALK", data['master'])
+            files = []
+            for root, dirs, xfiles in os.walk(local_path):
+                for name in xfiles:
+                    files.append(os.path.join( root, name ) )
+                for name in dirs:
+                    files.append(os.path.join( root, name ) + "/" ) 
+            
+            # NORMALIZZO I PERCORSI E LI RENDO RELATIVI
+            files = [ 
+                (f.replace("\\","/")).replace(local_path, "")[1:]
+                for f in files ]
+
+        master_to_s3(data['s3'], data['master'], files )
+
+
+
+
+def httpd_restart():
+    """Riavvia il servizio HTTPD"""
+    cmd = "sudo service httpd restart"
+    subprocess.check_call(cmd.split())
+
+
+def select_pc_type():
+    while True:
+        clear_screen()
+        print( """
+FIRST CONFIGURATION
+
+This computer is:
+
+    master = AWS S3 files wil be replaced with local files
+    slave  = Local files will be replaced with files stored on AWS S3
+
+""")
+
+        i = input( "Input (master/slave): ").lower()
+        if i not in ["slave" ,"master"]:
+            continue
+        else:
+            p = os.path.join(CURRENT_PATH, "u."+i ) 
+            with open( p, "w") as f:
+                print( "\n\nFile '{}' is been created!\n\nDelete this file to change configuration.\n".format(p) )
+            input( STR_CONTINUE )
+            return
+
+
+def menu(mode):
+
+    if mode == MASTER:
+        str_action = "UPLOAD"
+    else:
+        str_action = "DOWNLOAD"
+
+    while True:
+
+        clear_screen()
+
+        maps = load_maps()
+
+        if maps:
+            print("\n{}\n".format(str_action))
+            for n, m in enumerate( maps ):
+                print(" {:>3} = {}".format( n , m['name'] ) )
+
+            print("")
+            print(" all = {} ALL".format(str_action))
+            print("")
+            print("."*50)
+            print("")
+            print(" upm = Update Map")
+
+            if CONFIG['S3PScript']:
+                print(" ups = Update Script")
+
+            if CONFIG['AddRH']:
+                print("")
+                print("  rh = $ service HTTPD restart")
+
+            print("")
+            print("   x = Exit")
+
+            modulo = input("\nSelect: ").lower()
+
+        else:
+            modulo = "upm"
+
+
+        clear_screen()
+
+        if modulo == "upm":
+            update_maps()
+
+
+        elif CONFIG['S3PScript'] and modulo == "ups":
+            update_script()
+            print("DONE. Restart script.")
+            break
+
+        elif modulo == "x":
+            break
+
+        elif CONFIG['AddRH'] and  modulo == "rh":
+            httpd_restart()
+            input( STR_CONTINUE)
+
+        else:
+            if modulo == "all":
+                lista_aggiorna = [ x for x in range(1,len(maps))]
+            else:
+                try:
+                    modulo = int(modulo)
+                except:
+                    # invalid choice
+                    continue
+                else:
+                    if modulo >= 0 and modulo < len(maps):
+                        lista_aggiorna = [ modulo ]
+                    else:
+                        # invalid choice
+                        continue
+
+            for aggiorna in lista_aggiorna:
+                syncronize(maps[aggiorna], mode)
+
+            input( STR_CONTINUE )
+
+    print("\nBye\n")
+
+############
+
+def start():
+    global CONFIG
+    build_dir(DATA_PATH)
+    CONFIG = load_configuration()
+
+    clear_screen()
+    if os.path.isfile( os.path.join( CURRENT_PATH, "u.slave" ) ):
+        menu(SLAVE)
+
+    elif os.path.isfile( os.path.join( CURRENT_PATH, "u.master" ) ):
+        menu(MASTER)
+
+    else:
+        select_pc_type()
+        start()
+
+
+def test_upload():
+    b =  boto3.resource('s3', config=boto3.session.Config(signature_version='s3v4'))
+
+    bucket, prefix = split_bucket_prefix( "test.amecom.it/S23SROOT/xdir/" )
+
+    print(bucket, prefix) 
+    object = b.Object( bucket, prefix )
+#    if object.load():
+
+    with open("C:/diskstation/s3replica/AWS/S32S_DATA/TEST/root/f1.txt", "rb") as data:
+        request = object.put(
+                Body=data
+            )
+        return request['ResponseMetadata']['HTTPStatusCode'] == 200
+
+
+
+if __name__ == "__main__":
+    ### MAPPA >> amedeo.amecom.it/AWS/S32S_DATA/TEST/map
+
+    start()
+
