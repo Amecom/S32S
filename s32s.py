b6e5a4d4ff7eeda480e2bd663aba16ecdf81eead
Amecom <32417424+Amecom@users.noreply.github.com>
2017-11-14 05:48:04 +0100

    separeted txt string

--- s32s.py
+++ s32s.py
@@ -19,15 +19,64 @@ ISMASTER = None
 MAPS = None
 os.sep = "/"
 
-DEL_ALERT = """
+TXT_TRANSFER_INFO = """
+ TRANSFER INFORMATION:
+
+    * FROM {origin_info} ORIGIN : '{origin_path}'
+   >> TO {destination_info} DESTINATION : '{destination_path}'
+"""
+TXT_DELETE_WARNING = """
   ********* WARNING  ********* 
 
-  ALL OBJECTS STORED in {}: '{}'
+  ALL OBJECTS STORED in {info}: '{path}'
   WILL BE PERMANENTLY DELETE.
 
  ***************************** 
+"""
+TXT_RESTART = " Restart script."
+TXT_MODE_INFO = " *** MODE: {mode} | S3 MAPS PATH: {mapspath} ***"
+TXT_ACTION_MASTER = "REPLACE S3 OBJECT WITH LOCAL ONES"
+TXT_ACTION_SLAVE = "REPLACE LOCAL OBJECTS WITH THOSE STORED IN S3"
+TXT_NOTDO = " NOTHING DONE!"
+TXT_DO = " DONE!"
+TXT_LOADING_MAPS = " Loading maps... "
+TXT_REPLACE_ALL_OPTION = " all = REPLACE ALL"
+TXT_MENU_MODE = """
+SELECT RUN MODE:
 
- > Confirm permanent delete [y/n] ? """
+    {inp_master} = MASTER - {info_master}
+    {inp_slave} = SLAVE - {info_slave}
+"""
+TXT_MENU_OPTION = """
+ OPTIONS:
+
+  md = Maps Details
+  mr = Maps Reload
+  mo = Maps Open
+  sm = Switch to {mode}
+  up = Script Update
+   x = Exit
+"""
+
+TXT_INPUT = "\n > Enter input: "
+TXT_INPUT_OPTION = "\n > Enter input {option}: "
+TXT_INPUT_ENTER = "\n > Press ENTER to continue..."
+TXT_INPUT_INSERT_MAPS_PATH = "\n > Insert S3 path contains mapping files for '{mode}' [x to Exit]: "
+TXT_INPUT_DELETE_CONFIRM = " > Confirm permanent delete [y/n] ?" 
+
+TXT_ERR_CONFIGURATION_FAILS = " !! ERROR Configuration fails."
+TXT_ERR_S3_LIST_CONNECTION = " !! ERROR S3 PATH '{path}': bucket not found or access denied or no internet access."
+TXT_ERR_S3_LIST_CONTENTS = " !! ERROR S3 PATH '{path}': path does not have a contents or not exists."
+TXT_ERR_SCRIPT_UPDATE_CONFIG = " !! ERROR not found 'url_script_update' property in configuration file."
+TXT_ERR_MAP_INVALID = " !! ERROR IN MAPS"
+TXT_ERR_MAP_LINUX_USERHOME = " !! ERROR MAP {path}: Path can't start with ~"
+TXT_ERR_MAP_EMPTY = " !! ERROR EMPTY MAPS"
+TXT_ERR_MAP_EMPTY_NAME = " !! ERROR MAP '{filename}' list element number '{number}' does not have 'name' property."
+TXT_ERR_MAP_EMPTY_S3 = " !! ERROR MAP '{filename}' list element name '{name}' does not have 's3' property."
+TXT_ERR_MAP_EMPTY_MASTER = " !! ERROR MAP '{filename}' list element name '{name}' does not have 'master' property."
+TXT_ERR_MAP_EMPTY_SLAVE = " !! ERROR MAP '{filename}' list element name '{name}' does not have 'slave' property."
+TXT_DELETE_OBJECT = " {storage} DELETE: {object}"
+TXT_CREATE_OBJECT = " {storage} CREATE: {object}"
 
 
 def mode_name(ismaster):
@@ -37,11 +86,11 @@ def mode_name(ismaster):
 def str_action(ismaster=None):
     """Return string that describes transfert mode."""
     if not ismaster : ismaster = ISMASTER
-    return "REPLACE S3 OBJECT WITH LOCAL ONES" if ismaster else "REPLACE LOCAL OBJECTS WITH THOSE STORED IN S3"
+    return  TXT_ACTION_MASTER if ismaster else TXT_ACTION_SLAVE
 
 def enter_to_continue():
     """Create a break to allow the user to read output."""
-    input("\n > Press ENTER to continue...")
+    input(TXT_INPUT_ENTER)
 
 def clear():
     """Clear Screen."""
@@ -74,7 +123,8 @@ def slipt_s3path(path):
     """Split path into bucket and prefix.
 
     Return a tuple with bucket name and prefix.
-    Prefix can be a empty string."""
+    Prefix can be a empty string.
+    """
     p = path.split("/")
     bucket = p[0]
     prefix = "/".join( p[1:] ) if len(p) > 1 else ""
@@ -147,11 +197,11 @@ def ls_s3_path(path):
     try:
         objects = S3CLIENT.list_objects_v2( Bucket=bucket, Prefix=prefix)
     except:
-        print(" !! ERROR S3 PATH '{}': BUCKET NOT FOUND or ACCESS DENIED or NO INTERNET ACCESS.".format(path))
+        print(TXT_ERR_S3_LIST_CONNECTION.format(path=path))
         return []
 
     if objects.get("Contents") is None:
-        print(" !! ERROR S3 PATH '{}': PATH NOT HAVE CONTENTS OR NOT EXISTS.".format(path))
+        print(TXT_ERR_S3_LIST_CONTENTS.format(path=path))
         return []
 
     for object in objects["Contents"]:
@@ -175,7 +225,7 @@ def rm_slave_object(object_path):
 
 def rm_pc_object(object_path):
     """Drop all contents from local 'object_path'."""
-    print(" LOCAL DEL: {}".format( object_path ))
+    print(TXT_DELETE_OBJECT.format(storage="LOCAL", object=object_path ))
     if os.path.isdir(object_path):
         shutil.rmtree(object_path)
     elif os.path.isfile(object_path):
@@ -186,7 +236,7 @@ def rm_pc_object(object_path):
 
 def rm_s3_object(object_path):
     """Drop all contents from S3 'object_path'."""
-    print(" S3 DEL: {}".format(object_path))
+    print(TXT_DELETE_OBJECT.format(storage="S3", object=object_path ))
     bucket, prefix = slipt_s3path(object_path)
     b = S3RESOURCE.Bucket(bucket)
     try:
@@ -211,7 +261,7 @@ def mk_slave_object(object_path, data=None):
 
 def mk_pc_object(object_path, data=None):
     """Create object on local 'object_path'."""
-    print(" LOCAL CREATE: {}".format( object_path ))
+    print(TXT_CREATE_OBJECT.format(storage="LOCAL", object=object_path ))
     if data:
         # PATH IN THE SCRIPT ALWAYS MUST ENDS WITH "/"
         directory = "/".join(object_path.split("/")[:-1]) + "/"
@@ -233,7 +283,7 @@ def mk_pc_object(object_path, data=None):
 
 def mk_s3_object(object_path, data=None):
     """Create object on S3 'object_path'."""
-    print(" S3 CREATE: {}".format(object_path))
+    print(TXT_CREATE_OBJECT.format(storage="S3", object=object_path ))
     bucket, prefix = slipt_s3path(object_path)
     object = S3RESOURCE.Object( bucket, prefix )
     if data:
@@ -255,7 +305,7 @@ def update_script():
         #sleep(2)
         #urllib.request.urlretrieve(CONFIG['MAIN']['url_script_update'], script_path )
     else:
-        print("FILE CONFIG ERROR: not found 'url_script_update'")
+        print(TXT_ERR_SCRIPT_UPDATE_CONFIG)
         enter_to_continue()
         return False
 
@@ -271,7 +321,7 @@ def input_s3path_maps(save_configuration=True):
     """
     while True:
         clear()
-        i = input(" > Insert S3 path contains mapping files for '{}' [x to Exit]: ".format(mode_name(ISMASTER)))
+        i = input(TXT_INPUT_INSERT_MAPS_PATH.format(mode=mode_name(ISMASTER)))
         if i.lower() == "x":
             return
         i = normalize_external_path(i)
@@ -283,6 +333,7 @@ def input_s3path_maps(save_configuration=True):
             return maps
 
 def load_maps_from_s3path(path):
+    """Load all maps stored in S3 'path'."""
     maps = []
     for object in ls_s3_path(path):
         if object.endswith(".json"):
@@ -301,19 +352,24 @@ def load_maps_from_s3path(path):
                 maps = sorted(maps, key=lambda k:k['name'])
             return maps
 
-    print( "*** MAP ERROR ***" )
+    print( TXT_ERR_MAP_INVALID )
     enter_to_continue()
 
 
 def is_valid_maps(maps):
+    """Return True if maps is valid.
+    
+    If error print details.
+    """
 
     def check_path_error(path):
+        """check common error of 'path'"""
         if path[0] == "~":
-            return "path can't start with ~"
+            return TXT_ERR_MAP_LINUX_USERHOME
         return None
 
     if len(maps) == 0:
-        print("EMPTY MAPS") 
+        print(TXT_ERR_MAP_EMPTY) 
         return False
 
     for n, e in enumerate(maps):
@@ -324,31 +380,35 @@ def is_valid_maps(maps):
         slave_path = e.get("slave")
 
         if name is None:
-            print( "MAP ERROR Filename '{}' list element number '{}': empty 'name' property.".format(e['filename'], n) ) 
+            print( TXT_ERR_MAP_EMPTY_NAME.format(
+                filename=e['filename'],
+                number=n )) 
             break
 
         if s3_path is None:
-            print( "MAP ERROR Filename '{}' list element name '{}': empty 's3' property.".format(e['filename'], name) )
+            print( TXT_ERR_MAP_EMPTY_S3.format(
+                filename=e['filename'],
+                name=name ))
             break
 
         if ISMASTER:
             if master_path is None:
-                print( "MAP ERROR Filename '{}' list element name '{}': empty 'master' property.".format(e['filename'], name) )
+                print(TXT_ERR_MAP_EMPTY_MASTER.format(
+                    filename=e['filename'],
+                    name=name ))
                 break
 
-            err = check_path_error(master_path)
-            if err:
-                print( "MAP ERROR Filename '{}' list element name '{}': {}.".format(e['filename'], name, err) )
+            if check_path_error(master_path):
                 break
 
         else:
             if slave_path is None:
-                print( "MAP ERROR Filename '{}' list element name '{}': empty 'slave' property.".format(e['filename'], name) )
+                print(TXT_ERR_MAP_EMPTY_SLAVE.format(
+                    filename=e['filename'],
+                    name=name ))
                 break
 
-            err = check_path_error(slave_path)
-            if err:
-                print( "MAP ERROR Filename '{}' list element name '{}': {}.".format(e['filename'], name, err) )
+            if check_path_error(slave_path):
                 break
     else:
         # uscita dal ciclo senza errori
@@ -358,7 +418,7 @@ def is_valid_maps(maps):
 
 
 def transfer(xmap):
-
+    """Transfer data from master to slave using information path stored in 'xmap'."""
     if ISMASTER:
         origin_info = {
             'name' : 'master',
@@ -387,24 +447,23 @@ def transfer(xmap):
     if not objects: 
         objects = ls_master_path(origin)
 
-    print("""
- TRANSFER INFORMATION:
+    print(TXT_TRANSFER_INFO.format(
+        origin_info = ( origin_info['location'], origin_info['name'] ),
+        origin_path = origin,
+        destination_info = ( destination_info['location'], destination_info['name'] ),
+        destination_path = destination ))
 
-    * FROM {} ORIGIN : '{}'
-   >> TO {} DESTINATION : '{}'
-""".format(
-        ( origin_info['location'], origin_info['name'] ),
-        origin,
-        ( destination_info['location'], destination_info['name'] ),
-        destination ))
 
-    alert_text = DEL_ALERT.format(
-        ( destination_info['location'], destination_info['name'] ),
-        destination)
+    if not CONFIG['MAIN'].getboolean('skip_delete_alert'):
+        print( TXT_DELETE_WARNING.format(
+                        info = ( destination_info['location'], destination_info['name'] ),
+                        path = destination
+                    )
+              )
+        confirm = input(TXT_INPUT_DELETE_CONFIRM).lower()
+    else:
+        confirm = "y"
 
-    skip_alert = CONFIG['MAIN'].getboolean('skip_delete_alert')
-
-    confirm = "y" if skip_alert else input(alert_text).lower()
     if confirm == "y":
         rm_slave_object(destination)
         # need add '/' to 'mk_slave_object' destination because external path don't have
@@ -414,7 +473,7 @@ def transfer(xmap):
             mk_slave_object("/".join([destination, obj]), data)
 
     else:
-        print(" NOTHING DO!")
+        print(TXT_NOTDO)
 
 
 
@@ -425,29 +484,25 @@ def httpd_restart():
 
 
 def switch_mode():
+    """Switch program mode from MASTER and SLAVE and restart."""
     CONFIG['MAIN']['ismaster'] = str(not ISMASTER)
     save_config()
     main()
 
 
 def input_is_master():
-    # master deve essere 1 (true)
+    """Form to ask user the programm mode (MASTER/SLAVE)."""
+    # option value for master mast be "1"!
     option = ("1", "0")
     while True:
         clear()
-        print( """
-SELECT RUN MODE:
+        print( TXT_MENU_MODE.format(
+                inp_master=option[0],
+                info_master=str_action(True), 
+                inp_slave=option[1],
+                info_slave=str_action(False)))
 
-    {} = MASTER - {}
-    {} = SLAVE - {}
-
-""".format(
-    option[0],
-   str_action(True), 
-    option[1],
-   str_action(False) ))
-
-        i = input( " > Enter input {}: ".format(option))
+        i = input( TXT_INPUT_OPTION.format(option=option))
         if i not in option:
             continue
         else:
@@ -456,8 +511,9 @@ SELECT RUN MODE:
             break
     return bool(int(i))
 
-def show_maps():
 
+def show_maps():
+    """Show to user loaded maps details."""
     for n, m in enumerate( MAPS ):
         if ISMASTER:
             print("""\
@@ -475,37 +531,26 @@ def show_maps():
 
 
 def menu():
+    """Main user interface."""
     global MAPS
 
     while True:
 
         clear()
 
-        print(" *** MODE: {} | S3 MAPS PATH: {} ***".format(
-            mode_name(ISMASTER),
-            CONFIG[mode_name(ISMASTER)].get("s3_path_mapfiles")
-            )
-        )
+        print(TXT_MODE_INFO .format(
+            mode= mode_name(ISMASTER),
+            mapspath=CONFIG[mode_name(ISMASTER)].get("s3_path_mapfiles")))
 
-        print("""
- OPTIONS:
-
-  md = Maps Details
-  mr = Maps Reload
-  mo = Maps Open
-  sm = Switch to {} Mode
-  up = Script Update
-   x = Exit""".format( 
-        mode_name(not ISMASTER)
-    ))
+        print(TXT_MENU_OPTION.format(mode=mode_name(not ISMASTER)))
 
         print("\n {}:\n".format(str_action()))
         for n, m in enumerate( MAPS ):
             print(" {:>3} = {}".format( n , m['name'] ) )
 
-        print(" all = REPLACE ALL")
+        print(TXT_REPLACE_ALL_OPTION)
 
-        i = input("\n > Enter input: ").lower()
+        i = input(TXT_INPUT).lower()
 
         clear()
 
@@ -530,7 +575,8 @@ def menu():
 
         elif i == "up":
             if update_script():
-                print(" DONE. Restart script.")
+                print(TXT_DO)
+                print(TXT_RESTART)
                 break
 
         elif i == "rh":
@@ -561,64 +607,75 @@ def menu():
 
 
 def load_config():
+    """Load configuration and save it in global var CONFIG. 
+    
+    If configiguration file not exists create it with default values.
+    """
     global CONFIG
     if not CONFIG.read(CONFIG_FILENAME):
-        load_default_config()
+        CONFIG = default_config()
         save_config()
 
 
-def load_default_config():
-    global CONFIG
-    CONFIG['MAIN'] = {
+def default_config():
+    """return default configuration."""
+    config = {}
+    config['MAIN'] = {
         'url_script_update': "https://raw.githubusercontent.com/Amecom/S32Server/master/s32s.py",
         'reorder_map_elements': True,
         'skip_delete_alert': False,
         'ismaster': ''
         }
-    CONFIG[mode_name(True)] = {
+    config[mode_name(True)] = {
         's3_path_mapfiles': ''
         }
-    CONFIG[mode_name(False)] = {
+    config[mode_name(False)] = {
         's3_path_mapfiles': ''
         }
-
+    return config
 
 def save_config():
+    """Save configuration change."""
     with open(CONFIG_FILENAME, 'w') as configfile:
         CONFIG.write(configfile)
 
 
 def get_maps():
+    """Return maps. if not exists ask user for insert maps path."""
     path = CONFIG[mode_name(ISMASTER)].get("s3_path_mapfiles")
     if path:
         maps = load_maps_from_s3path(path)
         if maps:
             return maps
 
-    # se non è presente una mappa valida nel file di configurazione
+    # if not exists maps, ask for it
     return input_s3path_maps()
 
 
 def main():
+    """Main function"""
     global ISMASTER
     global MAPS
 
     clear()
-    load_config()
 
+    load_config()
     if CONFIG['MAIN'].get('ismaster'):
         ISMASTER = CONFIG['MAIN'].getboolean('ismaster')
     else:
         ISMASTER = input_is_master()
         clear()
 
-    print(" Loading maps... ")
+    print(TXT_LOADING_MAPS)
     maps = get_maps()
     if maps:
         MAPS = maps
         menu()
+
     else:
-        print(" *** Configuration fails, restart the script *** ")
+        print(TXT_ERR_CONFIGURATION_FAILS)
+        print(TXT_RESTART)
+
 
 if __name__ == "__main__":
     main()
