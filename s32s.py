e9bf8d36fc4a3b26096264aebff575a1a0af0410
Amecom <32417424+Amecom@users.noreply.github.com>
2017-11-14 04:19:25 +0100

    comment def

--- s32s.py
+++ s32s.py
@@ -31,64 +31,101 @@ DEL_ALERT = """
 
 
 def mode_name(ismaster):
-   return "MASTER" if ismaster else "SLAVE"
+    """Return string name used, for examples, in config."""
+    return "MASTER" if ismaster else "SLAVE"
 
 def str_action(ismaster=None):
+    """Return string that describes transfert mode."""
     if not ismaster : ismaster = ISMASTER
     return "REPLACE S3 OBJECT WITH LOCAL ONES" if ismaster else "REPLACE LOCAL OBJECTS WITH THOSE STORED IN S3"
 
-
-def stopscreen():
+def enter_to_continue():
+    """Create a break to allow the user to read output."""
     input("\n > Press ENTER to continue...")
 
-
 def clear():
-    """Pulisce lo schermo"""
+    """Clear Screen."""
     os.system('cls' if os.name=='nt' else 'clear')
 
 
-def filter_files(data:list):
-    """Regole di esclusione
-    dalla sincronizzazione file.
-    In questo esempio escludo sempre i file della cache di Python
+##def filter_files(data:list):
+##    """Regole di esclusione
+##    dalla sincronizzazione file.
+##    In questo esempio escludo sempre i file della cache di Python
+##    """
+##    return [ d for d in data if d.find("__pycache__") == -1 ]
+
+
+def normalize_external_path(path):
+    """Normalize 'path'.
+    
+    Return external path.
+    External paths, contrary to what happens internally,
+    NOT ENDS WITH / and
+    path separator is always /
     """
-    return [ d for d in data if d.find("__pycache__") == -1 ]
-
-
-def normalize_external_path(p):
-    # al contrario di come succede internamente
-    # i path directory provenienti dagli input esterni,
-    # percorsi di mapping
-    # non devono terminare con /
-    # e anche in windows il separatore di path deve essere  /
-    r = p.replace("\\","/")
+    r = path.replace("\\","/")
     if r.endswith("/"):
         r = r[:-1]
     return r
 
 
-def slipt_s3path(s3path):
-    """Return a tuple with bucket name and prefix from a given s3path.
-    """
-    s3_part = s3path.split("/")
-    bucket = s3_part[0]
-    prefix = "/".join( s3_part[1:] ) if len(s3_part) > 1 else ""
+def slipt_s3path(path):
+    """Split path into bucket and prefix.
+
+    Return a tuple with bucket name and prefix.
+    Prefix can be a empty string."""
+    p = path.split("/")
+    bucket = p[0]
+    prefix = "/".join( p[1:] ) if len(p) > 1 else ""
     return bucket, prefix
 
-def ls_master(path):
-    return ls_localpath(path) if ISMASTER else ls_s3path(path)
 
-def ls_slave(path):
-    return ls_s3path(path) if ISMASTER else ls_localpath(path)
+def get_master_file(filepath):
+    """Return binary data from 'filepath' in master repository."""
+    f = get_pc_file if ISMASTER else get_s3_file
+    return f(filepath)
 
-def ls_localpath(path):
+def get_slave_file(filepath):
+    """Return binary data from 'filepath' in slave repository."""
+    f = get_s3_file if ISMASTER else get_pc_file
+    return f(filepath)
+
+def get_pc_file(filepath):
+    """Return binary data from 'filepath' in local files."""
+    with open( filepath, "rb" ) as file:
+        return file.read()
+
+def get_s3_file(filepath):
+    """Return binary data from 'filepath' in S3 files."""
+    bucket, prefix = slipt_s3path(filepath)
+    file = S3RESOURCE.Object(bucket, prefix).get()
+    return file['Body'].read()
+
+
+def ls_master_path(path):
+    """Return a generator with list of objects stored in master repository.
+
+    If current run mode is master the master repository is local repository
+    otherwise is S3 repository
     """
-    Return generator of objects contained in 'path'.
+    return ls_pc_path(path) if ISMASTER else ls_s3_path(path)
 
-    Path objects are relative to 'path'
-    and directories endswith "/" char.
+def ls_slave_path(path):
+    """Return a generator with list of objects stored in slave repository
+
+    If current run mode is master the slave repository is local repository
+    otherwise is S3 repository.
     """
-        # con aggiunta di os.sep = "/" non dovrebbe essere piu necessario 'norm'...
+    return ls_s3_path(path) if ISMASTER else ls_pc_path(path)
+
+def ls_pc_path(path):
+    """Return generator of list of objects stored in local 'path'.
+
+    Objects's Path is relative to given 'path'
+    and directories ends with "/" char.
+    """
+        # norm function with os.sep = "/" ca be unnecessary
     norm = lambda p: p.replace("\\","/")
     base = norm(path)
     make_relative = lambda p: norm(p).replace(base, "")[1:]
@@ -98,22 +135,23 @@ def ls_localpath(path):
         for name in dirs:
             yield make_relative(os.path.join( root, name ) + "/") 
 
-def ls_s3path(s3path):
-    """
-    Return a generator of objects contained in 's3path'.
+def ls_s3_path(path):
+    """Return a generator of list objects stored in S3 'path'.
 
-    Path objects are relative to 's3path'
-    and directories endswith "/" char.
+    Objects Path are relative to 'path'
+    and directories ends with "/" char.
+
+    If error return empty list.
     """
-    bucket, prefix = slipt_s3path(s3path)
+    bucket, prefix = slipt_s3path(path)
     try:
         objects = S3CLIENT.list_objects_v2( Bucket=bucket, Prefix=prefix)
     except:
-        print(" !! S3 PATH '{}' CONNECTION ERROR: BUCKET NOT FOUND or ACCESS DENIED or NO INTERNET ACCESS.".format(s3path))
+        print(" !! ERROR S3 PATH '{}': BUCKET NOT FOUND or ACCESS DENIED or NO INTERNET ACCESS.".format(path))
         return []
 
     if objects.get("Contents") is None:
-        print(" !! S3 PATH '{}' ERROR: NO HAVE CONTENTS.".format(s3path))
+        print(" !! ERROR S3 PATH '{}': PATH NOT HAVE CONTENTS OR NOT EXISTS.".format(path))
         return []
 
     for object in objects["Contents"]:
@@ -124,48 +162,32 @@ def ls_s3path(s3path):
         if name != "":
             yield name
 
-################
 
-def get_master(path):
-    f = get_localfile if ISMASTER else get_s3file
-    return f(path)
+def rm_slave_object(object_path):
+    """Drop all contents from slave repository 'object_path'."""
+    f = rm_s3_object if ISMASTER else rm_pc_object
+    return f(object_path)
 
-def get_slave(path):
-    f = get_s3file if ISMASTER else get_localfile
-    return f(path)
+# UNUSED FUNCTION COMMENTED FOR SECURITY REASON
+#def rm_master_object(object_path):
+#    """Remove all contents from 'object_path' in master repository."""
+#    rm_pc_object(path) if ISMASTER else return rm_s3_object(path)
 
-def get_localfile(path):
-    with open( path, "rb" ) as file:
-        return file.read()
-
-def get_s3file(s3filepath):
-    bucket, prefix = slipt_s3path(s3filepath)
-    file = S3RESOURCE.Object(bucket, prefix).get()
-    return file['Body'].read()
-
-################
-
-def rm_slave(path):
-    f = rm_s3object if ISMASTER else rm_localobject
-    return f(path)
-
-# COMMENTATA PER NON SI DOVREBBE MAI USARE QUESTA FUNZIONE
-#def rm_master(path):
-#    rm_localobject(path) if ISMASTER else return rm_s3object(path)
-
-def rm_localobject(object):
-    print(" LOCAL DEL: {}".format( object ))
-    if os.path.isdir(object):
-        shutil.rmtree(object)
-    elif os.path.isfile(object):
-        os.remove(object)
+def rm_pc_object(object_path):
+    """Drop all contents from local 'object_path'."""
+    print(" LOCAL DEL: {}".format( object_path ))
+    if os.path.isdir(object_path):
+        shutil.rmtree(object_path)
+    elif os.path.isfile(object_path):
+        os.remove(object_path)
     # e' utile fare una pausa prima per permettere al sistema di rilasciare i files
     sleep(3)
     return True
 
-def rm_s3object(s3object):
-    print(" S3 DEL: {}".format( s3object ))
-    bucket, prefix = slipt_s3path(s3object)
+def rm_s3_object(object_path):
+    """Drop all contents from S3 'object_path'."""
+    print(" S3 DEL: {}".format(object_path))
+    bucket, prefix = slipt_s3path(object_path)
     b = S3RESOURCE.Bucket(bucket)
     try:
         request = b.objects.filter(Prefix=prefix).delete()
@@ -175,22 +197,26 @@ def rm_s3object(s3object):
     else:
         return True
 
-#############
-# COMMENTATA PER NON SI DOVREBBE MAI USARE QUESTA FUNZIONE
-#def mk_master(path, data=None):
-#    return mk_localpath(path, data=None) if ISMASTER else mk_s3path(path, data=None)
 
-def mk_slave(path, data=None):
-    f = mk_s3path if ISMASTER else mk_localpath
-    return f(path, data)
+# UNUSED FUNCTION COMMENTED FOR SECURITY REASON
+#def mk_master_object(object_path, data=None):
+#    """Create object in master repository 'object_path'."""
+#    f = mk_pc_object if ISMASTER else mk_s3_object
+#    return f(object_path, data)
 
-def mk_localpath(path, data=None):
-    print(" LOCAL CREATE: {}".format( path ))
+def mk_slave_object(object_path, data=None):
+    """Create object in slave repository 'object_path'."""
+    f = mk_s3_object if ISMASTER else mk_pc_object
+    return f(object_path, data)
+
+def mk_pc_object(object_path, data=None):
+    """Create object on local 'object_path'."""
+    print(" LOCAL CREATE: {}".format( object_path ))
     if data:
-        # internamente le directory terminano sempre con "/"
-        directory = "/".join(path.split("/")[:-1]) + "/"
+        # PATH IN THE SCRIPT ALWAYS MUST ENDS WITH "/"
+        directory = "/".join(object_path.split("/")[:-1]) + "/"
     else:
-        directory = path
+        directory = object_path
 
     if not os.path.exists(directory):
         try:
@@ -199,16 +225,16 @@ def mk_localpath(path, data=None):
             if e.errno != errno.EEXIST:
                 raise
     if data:
-        with open(path, "wb") as file:
+        with open(object_path, "wb") as file:
             file.write(data)
-    result = True
-    return result
+
+    return True
 
 
-def mk_s3path(s3object, data=None):
-    print(" S3 CREATE: {}".format(s3object))
-
-    bucket, prefix = slipt_s3path(s3object)
+def mk_s3_object(object_path, data=None):
+    """Create object on S3 'object_path'."""
+    print(" S3 CREATE: {}".format(object_path))
+    bucket, prefix = slipt_s3path(object_path)
     object = S3RESOURCE.Object( bucket, prefix )
     if data:
         request = object.put(Body=data)
@@ -219,37 +245,6 @@ def mk_s3path(s3object, data=None):
     return result
 
 
-def copy(origin, destination, objects):
-    print("""
- EXECUTE TRANSFER:
-
-    * FROM '{}' ORIGIN: '{}'
-   >> TO '{}' DESTINATION: '{}'
-""".format(
-        "LOCAL" if ISMASTER else "S3",
-        origin,
-        "S3" if ISMASTER else "LOCAL",
-        destination ))
-
-    alert_text = DEL_ALERT.format(
-        "S3 PATH" if ISMASTER else "LOCAL PATH",
-        destination)
-
-    confirm = "y" if CONFIG['MAIN'].getboolean('skip_delete_alert') else input(alert_text).lower()
-    if confirm == "y":
-        rm_slave(destination)
-        # ricreo la directory principale (le directory devono terminare con /
-        mk_slave(destination + "/")
-        for obj in objects:
-            data = None if obj.endswith("/") else get_master( "/".join([origin, obj]) )
-            ###if data is None:
-            ###    assert objendswith("/"), "If data; object filename can't be endswith /"
-            ###else:
-            ###    assert not obj.endswith("/"), "If data is None, object must be a directory. Directory ends with /"
-            mk_slave("/".join([destination, obj]), data)
-
-    else:
-        print("NOTHING DO")
 
 
 def update_script():
@@ -261,16 +256,17 @@ def update_script():
         #urllib.request.urlretrieve(CONFIG['MAIN']['url_script_update'], script_path )
     else:
         print("FILE CONFIG ERROR: not found 'url_script_update'")
-        stopscreen()
+        enter_to_continue()
         return False
 
     return True
 
 def input_s3path_maps(save_configuration=True):
-    """Ask user and save in configuration the s3path of mapping files
+    """Ask user and save in configuration the s3 path of mapping files
 
     Return: 
         - maps list
+            or
         - None (on error or user exit)
     """
     while True:
@@ -278,8 +274,7 @@ def input_s3path_maps(save_configuration=True):
         i = input(" > Insert S3 path contains mapping files for '{}' [x to Exit]: ".format(mode_name(ISMASTER)))
         if i.lower() == "x":
             return
-        if i.endswith("/"):
-            i = i[:-1]
+        i = normalize_external_path(i)
         maps = load_maps_from_s3path(i)
         if maps:
             if save_configuration:
@@ -287,11 +282,11 @@ def input_s3path_maps(save_configuration=True):
                 save_config()
             return maps
 
-def load_maps_from_s3path(s3path):
+def load_maps_from_s3path(path):
     maps = []
-    for object in ls_s3path(s3path):
+    for object in ls_s3_path(path):
         if object.endswith(".json"):
-            file = get_s3file( "/".join([s3path, object] ))
+            file = get_s3_file( "/".join([path, object] ))
             xmap = json.loads(file.decode('utf8'))
             for m in xmap:
                 m['filename'] = object
@@ -307,7 +302,7 @@ def load_maps_from_s3path(s3path):
             return maps
 
     print( "*** MAP ERROR ***" )
-    stopscreen()
+    enter_to_continue()
 
 
 def is_valid_maps(maps):
@@ -364,26 +359,67 @@ def is_valid_maps(maps):
 
 def transfer(xmap):
 
-    # se nella definizione è presente un lista di files
-    # sincronizzo solo quelli
-    # ATTENZIONE eventuali altri file presenti localmenti nella directory verranno comunque rimossi!
-    # Altrimenti sincronizzo tutti i file presenti nel percorso S3 specificato
-
     if ISMASTER:
-        origin = 'master'
-        destination = 's3'
+        origin_info = {
+            'name' : 'master',
+            'location' : 'LOCAL'
+            }
+        destination_info = {
+            'name' : 's3',
+            'location' : 'REMOTE'
+            }
     else:
-        origin = 's3'
-        destination = 'slave'
+        origin_info = {
+            'name' : 's3',
+            'location' : 'REMOTE'
+            }
+        destination_info = {
+            'name' : 'slave',
+            'location' : 'LOCAL'
+            }
+
+    # retrieve path from map property
+    origin = xmap[origin_info['name']] 
+    destination = xmap[destination_info['name']]
 
     objects = xmap.get('objects')
+    # if not objects, get all objects in origin path.
     if not objects: 
-        objects = ls_master(xmap[origin])
-    copy(xmap[origin], xmap[destination], objects)
+        objects = ls_master_path(origin)
+
+    print("""
+ TRANSFER INFORMATION:
+
+    * FROM {} ORIGIN : '{}'
+   >> TO {} DESTINATION : '{}'
+""".format(
+        ( origin_info['location'], origin_info['name'] ),
+        origin,
+        ( destination_info['location'], destination_info['name'] ),
+        destination ))
+
+    alert_text = DEL_ALERT.format(
+        ( destination_info['location'], destination_info['name'] ),
+        destination)
+
+    skip_alert = CONFIG['MAIN'].getboolean('skip_delete_alert')
+
+    confirm = "y" if skip_alert else input(alert_text).lower()
+    if confirm == "y":
+        rm_slave_object(destination)
+        # need add '/' to 'mk_slave_object' destination because external path don't have
+        mk_slave_object(destination + "/")
+        for obj in objects:
+            data = None if obj.endswith("/") else get_master_file( "/".join([origin, obj]) )
+            mk_slave_object("/".join([destination, obj]), data)
+
+    else:
+        print(" NOTHING DO!")
+
 
 
 def httpd_restart():
-    """Riavvia il servizio HTTPD"""
+    """Restart HTTPD service """
     cmd = "sudo service httpd restart"
     subprocess.check_call(cmd.split())
 
@@ -392,6 +428,7 @@ def switch_mode():
     CONFIG['MAIN']['ismaster'] = str(not ISMASTER)
     save_config()
     main()
+
 
 def input_is_master():
     # master deve essere 1 (true)
@@ -489,7 +526,7 @@ def menu():
 
         elif i == "md":
             show_maps()
-            stopscreen()
+            enter_to_continue()
 
         elif i == "up":
             if update_script():
@@ -498,7 +535,7 @@ def menu():
 
         elif i == "rh":
             httpd_restart()
-            stopscreen()
+            enter_to_continue()
 
         else:
 
@@ -520,7 +557,7 @@ def menu():
             for xmap in selected_xmap:
                 transfer(MAPS[xmap])
 
-            stopscreen()
+            enter_to_continue()
 
 
 def load_config():
@@ -548,13 +585,13 @@ def load_default_config():
 
 def save_config():
     with open(CONFIG_FILENAME, 'w') as configfile:
-      CONFIG.write(configfile)
+        CONFIG.write(configfile)
 
 
 def get_maps():
-    s3path = CONFIG[mode_name(ISMASTER)].get("s3_path_mapfiles")
-    if s3path:
-        maps = load_maps_from_s3path(s3path)
+    path = CONFIG[mode_name(ISMASTER)].get("s3_path_mapfiles")
+    if path:
+        maps = load_maps_from_s3path(path)
         if maps:
             return maps
 
